cadcd20e4694c00dd2e352069f67392a9be74287
Janusz Lewandowski <lew21@xtreeme.org>
2014-06-17 23:59:09 +0200

    Update RDF namespace to use X:IRIs.

--- libxtreeme/RDF.h
+++ libxtreeme/RDF.h
@@ -1,7 +1,7 @@
 #ifndef Xtreeme_RDF_h
 #define Xtreeme_RDF_h
 
-#include <QUrl>
+#include <libxtypes/IRI.h>
 
 namespace RDF
 {
@@ -9,46 +9,46 @@ namespace RDF
 	const QString BaseRDFS("http://www.w3.org/2000/01/rdf-schema#");
 
 	// 2. Classes
-	const QUrl Resource  (BaseRDFS + "Resource");
-	const QUrl Class     (BaseRDFS + "Class");
-	const QUrl Literal   (BaseRDFS + "Literal");
-	const QUrl Datatype  (BaseRDFS + "Datatype");
-	const QUrl XMLLiteral(BaseRDF  + "XMLLiteral");
-	const QUrl Property  (BaseRDF  + "Property");
+	const X::IRI Resource  (BaseRDFS + "Resource");
+	const X::IRI Class     (BaseRDFS + "Class");
+	const X::IRI Literal   (BaseRDFS + "Literal");
+	const X::IRI Datatype  (BaseRDFS + "Datatype");
+	const X::IRI XMLLiteral(BaseRDF  + "XMLLiteral");
+	const X::IRI Property  (BaseRDF  + "Property");
 
 	// 3. Properties
-	const QUrl range        (BaseRDFS + "range");
-	const QUrl domain       (BaseRDFS + "domain");
-	const QUrl type         (BaseRDF  + "type");
-	const QUrl subClassOf   (BaseRDFS + "subClassOf");
-	const QUrl subPropertyOf(BaseRDFS + "subPropertyOf");
-	const QUrl label        (BaseRDFS + "label");
-	const QUrl comment      (BaseRDFS + "comment");
+	const X::IRI range        (BaseRDFS + "range");
+	const X::IRI domain       (BaseRDFS + "domain");
+	const X::IRI type         (BaseRDF  + "type");
+	const X::IRI subClassOf   (BaseRDFS + "subClassOf");
+	const X::IRI subPropertyOf(BaseRDFS + "subPropertyOf");
+	const X::IRI label        (BaseRDFS + "label");
+	const X::IRI comment      (BaseRDFS + "comment");
 
 	// 5.1 Container Classes and Properties
-	const QUrl Container (BaseRDFS + "Container");
-	const QUrl Bag       (BaseRDF  + "Bag");
-	const QUrl Seq       (BaseRDF  + "Seq");
-	const QUrl Alt       (BaseRDF  + "Alt");
-	const QUrl ContainerMembershipProperty(BaseRDFS + "ContainerMembershipProperty");
-	const QUrl member    (BaseRDFS + "member");
+	const X::IRI Container (BaseRDFS + "Container");
+	const X::IRI Bag       (BaseRDF  + "Bag");
+	const X::IRI Seq       (BaseRDF  + "Seq");
+	const X::IRI Alt       (BaseRDF  + "Alt");
+	const X::IRI ContainerMembershipProperty(BaseRDFS + "ContainerMembershipProperty");
+	const X::IRI member    (BaseRDFS + "member");
 
 	// 5.2 RDF Collections
-	const QUrl List      (BaseRDF  + "List");
-	const QUrl first     (BaseRDF  + "first");
-	const QUrl rest      (BaseRDF  + "rest");
-	const QUrl nil       (BaseRDF  + "nil");
+	const X::IRI List      (BaseRDF  + "List");
+	const X::IRI first     (BaseRDF  + "first");
+	const X::IRI rest      (BaseRDF  + "rest");
+	const X::IRI nil       (BaseRDF  + "nil");
 
 	// 5.3 Reification Vocabulary
-	const QUrl Statement (BaseRDF  + "Statement");
-	const QUrl subject   (BaseRDF  + "subject");
-	const QUrl predicate (BaseRDF  + "predicate");
-	const QUrl object    (BaseRDF  + "object");
+	const X::IRI Statement (BaseRDF  + "Statement");
+	const X::IRI subject   (BaseRDF  + "subject");
+	const X::IRI predicate (BaseRDF  + "predicate");
+	const X::IRI object    (BaseRDF  + "object");
 
 	// 5.4 Utility Properties
-	const QUrl seeAlso    (BaseRDFS + "seeAlso");
-	const QUrl isDefinedBy(BaseRDFS + "isDefinedBy");
-	const QUrl value      (BaseRDF  + "value");
+	const X::IRI seeAlso    (BaseRDFS + "seeAlso");
+	const X::IRI isDefinedBy(BaseRDFS + "isDefinedBy");
+	const X::IRI value      (BaseRDF  + "value");
 }
 
 #endif
